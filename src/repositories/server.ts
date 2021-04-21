faf026c33a98cb464b1c12063758dcdb63c9f0be
jilherme <rosa.guilhermediogo@gmail.com>
2021-04-21 02:04:34 -0300

    server.ts movido e modificado

---
+++ src/repositories/server.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+
+import "./database";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json)
+
+app.use(routes);
+
+app.listen(3333, () => console.log("server is running on port 3333"));
